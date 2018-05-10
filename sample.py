bc98617e28899a1b98db9ba5e62d3de468b1e5eb
Kevin Corbin <kecorbin@cisco.com>
2018-05-10 15:12:55 -0500

    update sample to include Response

--- sample.py
+++ sample.py
@@ -5,6 +5,8 @@ Sample code for using ciscosparkbot
 
 import os
 from ciscosparkbot import SparkBot
+from ciscosparkbot.models import Response
+from ciscosparkapi import Message
 
 __author__ = "imapex"
 __author_email__ = "CiscoSparkBot@imapex.io"
@@ -26,6 +28,10 @@ def do_something(incoming_msg):
     """
     return "i did what you said - {}".format(incoming_msg.text)
 
+def ret_message(incoming_msg):
+    m = Response()
+    m.files = 'https://sayingimages.com/wp-content/uploads/aaaaaalll-righty-then-alrighty-meme.jpg'
+    return m
 
 # Create a new bot
 bot = SparkBot(bot_app_name, spark_bot_token=spark_token,
@@ -33,6 +39,7 @@ bot = SparkBot(bot_app_name, spark_bot_token=spark_token,
 
 # Add new command
 bot.add_command('/dosomething', 'help for do something', do_something)
+bot.add_command('/demo', 'sampel that allows spark message to be returned', ret_message)
 
 # Run Bot
 bot.run(host='0.0.0.0', port=5000)
